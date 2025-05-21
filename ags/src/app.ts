300d6df1c41a094779c3247ed06fb70d2dea4e26
Dalton Kye Miller <hello@daltons.email>
2025-05-21 07:28:28 -0500

    begin ags tailwind rewrite

--- ags/src/app.ts
+++ ags/src/app.ts
@@ -1,11 +1,12 @@
 import { App } from "astal/gtk4";
-import style from "./style/main.scss";
+// import style from "./style/main.scss";
 import AppLauncher from "./widget/app-launcher";
 import Bar from "./widget/bar";
 import NotificationPopups from "./widget/notification/notification-popup";
 import { setupHyprland } from "./lib/hyprland";
 import BgBlur from "./widget/bg-blur";
 import Clipboard from "./widget/clipboard";
+import style from "./output.css"
 
 App.start({
   css: style,
